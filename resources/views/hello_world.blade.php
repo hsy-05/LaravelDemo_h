29a3c8236f3eb4f5a5ff37023f333342fbbe66c6
hsy-05 <107b11602@mailst.cjcu.edu.tw>
2020-12-18 20:27:22 +0800

    first commit

---
+++ resources/views/hello_world.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <meta charset="utf-8">
+
+        <title>Hello world!!!</title>
+        <style>
+            div {
+                background-color: yellow;
+            }
+        </style>
+
+    </head>
+
+    <body>
+
+      <div>Hello World!!!</div>
+
+    </body>
+
+</html> 
